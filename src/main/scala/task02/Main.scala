cd0962ec500eacd5e819a364b51ad37de97cfec3
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-03-17 10:01:20 +0100

    feat(Main.scala): implement functions to get the sign of an integer and negate a predicate on strings
    The changes in this commit include:
    - Implementing the function `getTheSignMethodStyle` using match-cases to return "positive" if the input integer is greater than or equal to 0, and "negative" otherwise.
    - Implementing the function `getTheSignLambdaStyle` as a val assigned to a function literal using match-cases to achieve the same functionality as `getTheSignMethodStyle`.
    - Implementing the function `negMethodStyle` to accept a predicate on strings and return another predicate on strings that does the exact opposite.
    - Implementing the function `negLambdaStyle` as a val assigned to a function literal to achieve the same functionality as `negMethodStyle`.
    - Adding a val `emptyString` to represent a predicate on strings that checks if the input string is empty.
    - Adding a print statement to test the `emptyString` predicate.
    
    These changes were made to fulfill the requirements of the task and provide the desired functionality.

--- src/main/scala/task02/Main.scala
+++ src/main/scala/task02/Main.scala
@@ -2,6 +2,13 @@ package task02
 
 object Main extends App:
 
+// a) 
+// Using match-cases, implement the following function from Int to
+// String: positive(x) =
+// (“positive” if x ≥ 0 “negative” if x < 0
+// in both of the following styles: (i) val assigned to function literal
+// (lambda) and (ii) method syntax.
+
     def getTheSignMethodStyle(x: Int): String = x match
         case x if x >= 0 => "positive"
         case _ => "negative"
@@ -9,3 +16,27 @@ object Main extends App:
     val getTheSignLambdaStyle: Int => String = _ match
         case x if x >= 0 => "positive"
         case _ => "negative"
+
+
+// b)
+// Implement a neg function that accepts a predicate on strings (i.e., a
+// function from strings to Booleans) and returns another predicate on strings, namely, one that does the exact opposite;
+// write the type first, and then define the function both as a val lambda and with method syntax val empty:
+// String => Boolean = _ == "" // predicate on strings
+// val notEmpty = neg(empty) // which type of notEmpty?
+// notEmpty("foo") // true
+// notEmpty("") // false
+// notEmpty("foo") && !notEmpty("") // true.. a comprehensive test
+
+    val emptyString: String => Boolean = _ == ""
+
+    println(emptyString(""))
+
+    def negMethodStyle (s: String => Boolean) : String => Boolean = !s(_)
+
+    val negLambdaStyle: (String => Boolean) => String => Boolean = s => !s(_)
+
+
+
+
+    
