6dbdaaef6909908e9cb0ebc4281c37d52ad8b9c6
Tommaso Patriti <tommaso.patriti@studio.unibo.it>
2024-03-17 10:29:28 +0100

    feat(Main.scala): add implementation for positive function using match-cases to determine if a number is positive or negative feat(Main.scala): add implementation for neg function that accepts a predicate on strings and returns the opposite predicate feat(Main.scala): make neg function work for generic predicates and write tests to check it feat(Main.scala): implement predicates to check if arguments x, y, z respect the relation x ≤ y = z in 4 variants (curried/non-curried × val/def)

--- src/main/scala/task02/Main.scala
+++ src/main/scala/task02/Main.scala
@@ -2,7 +2,7 @@ package task02
 
 object Main extends App:
 
-// a) 
+// 3a) 
 // Using match-cases, implement the following function from Int to
 // String: positive(x) =
 // (“positive” if x ≥ 0 “negative” if x < 0
@@ -18,7 +18,7 @@ object Main extends App:
         case _ => "negative"
 
 
-// b)
+// 3b)
 // Implement a neg function that accepts a predicate on strings (i.e., a
 // function from strings to Booleans) and returns another predicate on strings, namely, one that does the exact opposite;
 // write the type first, and then define the function both as a val lambda and with method syntax val empty:
@@ -35,6 +35,27 @@ object Main extends App:
     def negMethodStyle (s: String => Boolean) : String => Boolean = !s(_)
 
     val negLambdaStyle: (String => Boolean) => String => Boolean = s => !s(_)
+
+//3c)
+//Make neg work for generic predicates, and write tests to check it (therefore, neg will be generic: def neg[X]...).
+    
+    def negMethodStyleGeneric[A] (s: A => Boolean) : A => Boolean = !s(_)
+
+
+
+//4) Currying
+// Implement a predicate that checks whether its arguments x,y,z respect
+// the relation x ≤ y = z, in 4 variants (curried/non-curried × val/def)
+
+    def xyzStandard(x: Double, y: Double, z: Double): Boolean = x <= y || y == z 
+
+    def xyzCurryed(x: Double) (y: Double) (z: Double): Boolean = x <= y || y == z 
+
+    val xyzLambdaCurryed: Double => Double => Double => Boolean = x => y => z => x <= y || y == z 
+
+
+
+
 
 
 
